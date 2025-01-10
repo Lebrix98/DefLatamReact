760e38b2bcc0755bd45895cb922e9085dbeaf60e
Jorge Diaz <jorgediazl1998@gmail.com>
2025-01-10 01:36:43 -0300

    Adavance Hito 3

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
       {/* <Home icons={icons} /> */}
       {/* <Register /> */}
       {/* <Login /> */}
-      <Cart2 />
+      <Cart />
       <Footer />
     </>
   );
--- src/components/Cart/Cart2.jsx
+++ src/components/Cart/Cart2.jsx
@@ -4,32 +4,50 @@ import { Capitalize } from "../../Helpers/functions";
 import "./style.css";
 
 export const Cart2 = () => {
-  const [cart, setCart] = useState(pizzaCart);
-
-  const [num1, setNum1] = useState(0);
-  const [num2, setNum2] = useState(0);
-  const [num3, setNum3] = useState(0);
-
+  const [cart, setCart] = useState({});
 
   const handleAdd = (arr) => {
-
-    console.log(arr)
-
-    if (arr.id === "P001") {
-      setNum1(num1 + 1);
-    } else if (arr.id === "P002") {
-      setNum2(arr.count + 1);
-    } else {
-      setNum3(arr.count + 1);
-    }
+    setCart((event) => {
+      if (event[arr.id]) {
+        return {
+          ...event,
+          [arr.id]: { ...event[arr.id], count: event[arr.id].count + 1 },
+        };
+      }
+      return {
+        ...event,
+        [arr.id]: { ...arr, count: 1 },
+      };
+    });
   };
 
-  const handleSubstract = (arr) => {};
+  const handleSubstract = (arr) => {
+    setCart((event) => {
+      if (event[arr.id] && event[arr.id].count > 1) {
+        return {
+          ...event,
+          [arr.id]: { ...event[arr.id], count: event[arr.id].count - 1 },
+        };
+      }
+      const updateEvent = { ...event };
+      // Borra para empezar desde cero
+      delete updateEvent[arr.id];
+      return updateEvent;
+    });
+  };
+
+  const total = Object.values(cart).reduce((prev, current) =>
+    prev + current.count * current.price, 0
+  );
+
+  console.log(total);
 
   return (
     <div className="Container_cart">
       <h1 className="title_cart">Detalles del Pedido:</h1>
       {pizzaCart.map((arr) => {
+        const value = cart[arr.id] || { count: 0 };
+
         return (
           <div className="Content_cart" key={arr.id}>
             <img className="img_pizza" src={arr.img} alt={arr.name} />
@@ -41,14 +59,14 @@ export const Cart2 = () => {
             >
               -
             </button>
-            <h4 className="count_pizza">{arr.count}</h4>
+            <h4 className="count_pizza">{value.count}</h4>
             <button className="btn_pizza add" onClick={() => handleAdd(arr)}>
               +
             </button>
           </div>
         );
       })}
-      <h2 className="Total_pizza">Total: ???</h2>
+      <h2 className="Total_pizza">Total: $ {total.toLocaleString()}</h2>
       <button className="btn_Pay">Pagar</button>
     </div>
   );
